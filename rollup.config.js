adf795e5e1a94049558107cf2dbe05f8ef94cc16
Popescu Vasile <49597813+7Hero@users.noreply.github.com>
2023-03-21 15:55:57 +0200

    fixed rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,32 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
 
-export default {
-    input: 'src/main.ts',
-    output: [
-        {
-            file: `./dist/cjs/index.js`,
-            format: 'cjs',
-            sourcemap: true,
-            name: 'react-spring-motion'
-        },
-        {
-            file: `./dist/esm/index.js`,
-            format: 'esm',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        external(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.json' }),
-        terser()
-    ]
-}
+const config = [
+    {
+        input: 'src/main.ts',
+        output: [
+            {
+                file: `./dist/index.js`,
+                format: 'esm',
+                sourcemap: true,
+                name: 'react-spring-motion'
+            },
+        ],
+        plugins: [
+            external(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            terser()
+        ]
+    },
+    {
+        input: './dist/dts/main.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()],
+    }
+]
+
+export default config;
